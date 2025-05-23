d61df24940a3e6a4daa3edaf18eae34e49f5f232
default <yunus.emre.guzel@mths.ca>
2025-05-23 13:53:30 +0000

    finshed the progrma

--- answer.php
+++ answer.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Volume of a sphere" />
+  <meta name="description" content="Volume of right rectangular pyramid  " />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Emre Guzel" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
@@ -18,38 +18,38 @@
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
   <link rel="manifest" href="/site.webmanifest">
   <link rel="stylesheet" href="css/style.css">
-  <title>Volume of a sphere </title>
+  <title>Volume of Right Rectangular Pyramid </title>
 </head>
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script src="js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Volume of a sphere</span>
+        <span class="mdl-layout-title">Volume of Right Rectangular Pyramid </span>
       </div>
     </header>
-    <h2 class="formula">Formula</h2>
-    <p class="formula-body">V = 4/3 π r³</p>
-    <br>
-  </div>
-  <br><br>
-  <?php
- // Safely get the radius from the query string
-  $radius = isset($_GET['radius']) ? floatval($_GET['radius']) : null;
-
-  // Calculate the volume
- $volume = (4 / 3) * pi() * pow($radius, 3);
-  $volumeRounded = round($volume, 2);
-  
-  // results
-  $dimensions = "<ul>\n<li>Volume is " . $volumeRounded . " cm³</li>\n</ul>";
-  echo "Volume is:<br>" ."<br>"."<br>". "<br>"."<br>" . $dimensions;
-  ?>
-
-  <br>
-  <img class="Calculate" src="images/volume-of-sphere.png" alt="Formula of sphere">
+    <main class="mdl-layout__content">
+      <div class="right-image">
+      </div>
+      <br><br>
+      <?php
+      $radiusOfSphere = $_GET["radius-of-sphere"];
+      $volume = (4 / 3) * pi() * pow($radiusOfSphere, 3);
+      echo "If sphere has radius = " . $radiusOfSphere;
+      echo "<br />";
+      echo "<br />";
+      echo "The Volume of the pyramid is: " . round($volume);
+      ?>
+      <br /> <br>
+      <div class="page-content-answer">
+        <a href="./index.php">Return ...</a>
+      </div>
+      <br>
+      <div class="Calculate-2">
+        <img src="./images/volume-of-sphere.png" alt="sphere">
+      </div>
+    </main>
 </body>
 
 </html>
